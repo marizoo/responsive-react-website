c3f99045522c1ebb9390b6b742f8938e6faeebbc
marizoo <marizoo@meow.woof>
2022-02-12 13:14:58 +0700

    component : Brand : 100%

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './brandStyle.css'
+import React from "react";
+import "./brandStyle.css";
+import { google, atlassian, dropbox, shopify, slack } from "./import";
 
 const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+    return (
+        <div className="brand section__padding">
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={atlassian} alt="google" />
+            </div>
+            <div>
+                <img src={dropbox} alt="google" />
+            </div>
+            <div>
+                <img src={shopify} alt="google" />
+            </div>
+            <div>
+                <img src={slack} alt="google" />
+            </div>
+        </div>
+    );
+};
 
-export default Brand
+export default Brand;
---
+++ src/components/brand/import.js
@@ -0,0 +1,7 @@
+import atlassian from "../../images/atlassian.png";
+import dropbox from "../../images/dropbox.png";
+import google from "../../images/google.png";
+import shopify from "../../images/shopify.png";
+import slack from "../../images/slack.png";
+
+export { google, atlassian, dropbox, shopify, slack };
