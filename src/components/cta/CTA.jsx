3b140aee56e8b33b8744d117c226031820fd3b6b
marizoo <marizoo@meow.woof>
2022-02-13 16:10:35 +0700

    section : Cta : 100%

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './ctaStyle.css'
+import React from "react";
+import "./ctaStyle.css";
 
 const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+    return (
+        <div className="cta">
+            <div className="cta-content">
+                <p>Get early access to the platform</p>
+                <h3>Register today & start exploring your opportunity</h3>
+            </div>
+            <div className="cta-btn">
+                <button type="button">Get Started</button>
+            </div>
+        </div>
+    );
+};
 
-export default CTA
+export default CTA;
